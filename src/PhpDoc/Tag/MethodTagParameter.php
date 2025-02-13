42b7a4a967d7830fbb0284e49c5cb7350a3ae1bc
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 22:10:02 +0000

    deploy: 1.9.0

---
+++ src/PhpDoc/Tag/MethodTagParameter.php
@@ -0,0 +1,50 @@
+<?php 
+
+namespace PHPStan\PhpDoc\Tag;
+return;
+
+use PHPStan\Reflection\PassedByReference;
+use PHPStan\Type\Type;
+
+/**
+ * @api
+ */
+final class MethodTagParameter
+{
+
+	public function __construct(
+		private Type $type,
+		private PassedByReference $passedByReference,
+		private bool $isOptional,
+		private bool $isVariadic,
+		private ?Type $defaultValue,
+	)
+	{
+	}
+
+	public function getType(): Type
+	{
+		return $this->type;
+	}
+
+	public function passedByReference(): PassedByReference
+	{
+		return $this->passedByReference;
+	}
+
+	public function isOptional(): bool
+	{
+		return $this->isOptional;
+	}
+
+	public function isVariadic(): bool
+	{
+		return $this->isVariadic;
+	}
+
+	public function getDefaultValue(): ?Type
+	{
+		return $this->defaultValue;
+	}
+
+}
