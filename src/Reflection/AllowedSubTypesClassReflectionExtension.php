4e8736af24a009487a0e1d4927239be4a2a3576c
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 22:20:57 +0000

    deploy: 1.9.2

---
+++ src/Reflection/AllowedSubTypesClassReflectionExtension.php
@@ -0,0 +1,37 @@
+<?php 
+
+namespace PHPStan\Reflection;
+return;
+
+use PHPStan\Type\Type;
+
+/**
+ * This is the extension interface to implement if you want to described
+ * allowed subtypes - to limit which classes can implement a certain interface
+ * or extend a certain parent class.
+ *
+ * To register it in the configuration file use the `phpstan.broker.allowedSubTypesClassReflectionExtension` service tag:
+ *
+ * ```
+ * services:
+ * 	-
+ *		class: App\PHPStan\MyExtension
+ *		tags:
+ *			- phpstan.broker.allowedSubTypesClassReflectionExtension
+ * ```
+ *
+ * Learn more: https://phpstan.org/developing-extensions/allowed-subtypes
+ *
+ * @api
+ */
+interface AllowedSubTypesClassReflectionExtension
+{
+
+	public function supports(ClassReflection $classReflection): bool;
+
+	/**
+	 * @return array<Type>
+	 */
+	public function getAllowedSubTypes(ClassReflection $classReflection): array;
+
+}
