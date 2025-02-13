557746837250c79e3b16282476b4becf3cbcda10
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 20:06:50 +0000

    deploy: 1.10.6

---
+++ src/Broker/FunctionNotFoundException.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace PHPStan\Broker;
+return;
+
+use PHPStan\AnalysedCodeException;
+use function sprintf;
+
+final class FunctionNotFoundException extends AnalysedCodeException
+{
+
+	public function __construct(private string $functionName)
+	{
+		parent::__construct(sprintf('Function %s not found while trying to analyse it - discovering symbols is probably not configured properly.', $functionName));
+	}
+
+	public function getFunctionName(): string
+	{
+		return $this->functionName;
+	}
+
+	public function getTip(): string
+	{
+		return 'Learn more at https://phpstan.org/user-guide/discovering-symbols';
+	}
+
+}
