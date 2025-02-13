107b205284502d229035730829671f3205c5804f
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 20:52:46 +0000

    deploy: 1.3.2

---
+++ src/Reflection/FunctionVariant.php
@@ -0,0 +1,67 @@
+<?php 
+
+namespace PHPStan\Reflection;
+return;
+
+use PHPStan\Type\Generic\TemplateTypeMap;
+use PHPStan\Type\Generic\TemplateTypeVarianceMap;
+use PHPStan\Type\Type;
+
+/**
+ * @api
+ */
+class FunctionVariant implements ParametersAcceptor
+{
+
+	private TemplateTypeVarianceMap $callSiteVarianceMap;
+
+	/**
+	 * @api
+	 * @param list<ParameterReflection> $parameters
+	 */
+	public function __construct(
+		private TemplateTypeMap $templateTypeMap,
+		private ?TemplateTypeMap $resolvedTemplateTypeMap,
+		private array $parameters,
+		private bool $isVariadic,
+		private Type $returnType,
+		?TemplateTypeVarianceMap $callSiteVarianceMap = null,
+	)
+	{
+		$this->callSiteVarianceMap = $callSiteVarianceMap ?? TemplateTypeVarianceMap::createEmpty();
+	}
+
+	public function getTemplateTypeMap(): TemplateTypeMap
+	{
+		return $this->templateTypeMap;
+	}
+
+	public function getResolvedTemplateTypeMap(): TemplateTypeMap
+	{
+		return $this->resolvedTemplateTypeMap ?? TemplateTypeMap::createEmpty();
+	}
+
+	public function getCallSiteVarianceMap(): TemplateTypeVarianceMap
+	{
+		return $this->callSiteVarianceMap;
+	}
+
+	/**
+	 * @return list<ParameterReflection>
+	 */
+	public function getParameters(): array
+	{
+		return $this->parameters;
+	}
+
+	public function isVariadic(): bool
+	{
+		return $this->isVariadic;
+	}
+
+	public function getReturnType(): Type
+	{
+		return $this->returnType;
+	}
+
+}
