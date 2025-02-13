ef818d63314a2a79d14b5d3f6d20dd9abb650306
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 20:42:46 +0000

    deploy: 1.12.3

---
+++ src/Rules/DeadCode/PossiblyPureFuncCallCollector.php
@@ -0,0 +1,51 @@
+<?php 
+
+namespace PHPStan\Rules\DeadCode;
+return;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Expression;
+use PHPStan\Analyser\Scope;
+use PHPStan\Collectors\Collector;
+use PHPStan\Reflection\ReflectionProvider;
+
+/**
+ * @implements Collector<Node\Stmt\Expression, array{string, int}>
+ */
+final class PossiblyPureFuncCallCollector implements Collector
+{
+
+	public function __construct(private ReflectionProvider $reflectionProvider)
+	{
+	}
+
+	public function getNodeType(): string
+	{
+		return Expression::class;
+	}
+
+	public function processNode(Node $node, Scope $scope)
+	{
+		if (!$node->expr instanceof Node\Expr\FuncCall) {
+			return null;
+		}
+		if (!$node->expr->name instanceof Node\Name) {
+			return null;
+		}
+
+		if (!$this->reflectionProvider->hasFunction($node->expr->name, $scope)) {
+			return null;
+		}
+
+		$functionReflection = $this->reflectionProvider->getFunction($node->expr->name, $scope);
+		if (!$functionReflection->isPure()->maybe()) {
+			return null;
+		}
+		if (!$functionReflection->hasSideEffects()->maybe()) {
+			return null;
+		}
+
+		return [$functionReflection->getName(), $node->getStartLine()];
+	}
+
+}
---
+++ src/Type/Generic/TemplateObjectWithoutClassType.php
@@ -0,0 +1,40 @@
+<?php 
+
+namespace PHPStan\Type\Generic;
+return;
+
+use PHPStan\Type\ObjectWithoutClassType;
+use PHPStan\Type\Traits\UndecidedComparisonCompoundTypeTrait;
+use PHPStan\Type\Type;
+
+/** @api */
+class TemplateObjectWithoutClassType extends ObjectWithoutClassType implements TemplateType
+{
+
+	use UndecidedComparisonCompoundTypeTrait;
+	/** @use TemplateTypeTrait<ObjectWithoutClassType> */
+	use TemplateTypeTrait;
+
+	/**
+	 * @param non-empty-string $name
+	 */
+	public function __construct(
+		TemplateTypeScope $scope,
+		TemplateTypeStrategy $templateTypeStrategy,
+		TemplateTypeVariance $templateTypeVariance,
+		string $name,
+		ObjectWithoutClassType $bound,
+		?Type $default,
+	)
+	{
+		parent::__construct();
+
+		$this->scope = $scope;
+		$this->strategy = $templateTypeStrategy;
+		$this->variance = $templateTypeVariance;
+		$this->name = $name;
+		$this->bound = $bound;
+		$this->default = $default;
+	}
+
+}
