fb30eb2c7d012c597db068ba1fcfac774477f5fe
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 22:04:58 +0000

    deploy: 1.8.5

---
+++ src/PhpDoc/TypeNodeResolverAwareExtension.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace PHPStan\PhpDoc;
+return;
+
+/** @api */
+interface TypeNodeResolverAwareExtension
+{
+
+	public function setTypeNodeResolver(TypeNodeResolver $typeNodeResolver): void;
+
+}
---
+++ src/Rules/PhpDoc/InvalidPhpDocTagValueRule.php
@@ -0,0 +1,101 @@
+<?php 
+
+namespace PHPStan\Rules\PhpDoc;
+return;
+
+use PhpParser\Node;
+use PhpParser\NodeAbstract;
+use PHPStan\Analyser\Scope;
+use PHPStan\Node\VirtualNode;
+use PHPStan\PhpDocParser\Ast\PhpDoc\InvalidTagValueNode;
+use PHPStan\PhpDocParser\Ast\PhpDoc\TypeAliasTagValueNode;
+use PHPStan\PhpDocParser\Ast\Type\InvalidTypeNode;
+use PHPStan\PhpDocParser\Lexer\Lexer;
+use PHPStan\PhpDocParser\Parser\PhpDocParser;
+use PHPStan\PhpDocParser\Parser\TokenIterator;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
+use function sprintf;
+use function str_starts_with;
+
+/**
+ * @implements Rule<NodeAbstract>
+ */
+final class InvalidPhpDocTagValueRule implements Rule
+{
+
+	public function __construct(
+		private Lexer $phpDocLexer,
+		private PhpDocParser $phpDocParser,
+	)
+	{
+	}
+
+	public function getNodeType(): string
+	{
+		return NodeAbstract::class;
+	}
+
+	public function processNode(Node $node, Scope $scope): array
+	{
+		// mirrored with InvalidPHPStanDocTagRule
+		if ($node instanceof VirtualNode) {
+			return [];
+		}
+		if (!$node instanceof Node\Stmt && !$node instanceof Node\PropertyHook) {
+			return [];
+		}
+		if ($node instanceof Node\Stmt\Expression) {
+			if (!$node->expr instanceof Node\Expr\Assign && !$node->expr instanceof Node\Expr\AssignRef) {
+				return [];
+			}
+		}
+
+		$docComment = $node->getDocComment();
+		if ($docComment === null) {
+			return [];
+		}
+
+		$phpDocString = $docComment->getText();
+		$tokens = new TokenIterator($this->phpDocLexer->tokenize($phpDocString));
+		$phpDocNode = $this->phpDocParser->parse($tokens);
+
+		$errors = [];
+		foreach ($phpDocNode->getTags() as $phpDocTag) {
+			if (str_starts_with($phpDocTag->name, '@phan-') || str_starts_with($phpDocTag->name, '@psalm-')) {
+				continue;
+			}
+
+			if ($phpDocTag->value instanceof TypeAliasTagValueNode) {
+				if (!$phpDocTag->value->type instanceof InvalidTypeNode) {
+					continue;
+				}
+
+				$errors[] = RuleErrorBuilder::message(sprintf(
+					'PHPDoc tag %s %s has invalid value: %s',
+					$phpDocTag->name,
+					$phpDocTag->value->alias,
+					$phpDocTag->value->type->getException()->getMessage(),
+				))
+					->line(PhpDocLineHelper::detectLine($node, $phpDocTag))
+					->identifier('phpDoc.parseError')->build();
+
+				continue;
+			} elseif (!($phpDocTag->value instanceof InvalidTagValueNode)) {
+				continue;
+			}
+
+			$errors[] = RuleErrorBuilder::message(sprintf(
+				'PHPDoc tag %s has invalid value (%s): %s',
+				$phpDocTag->name,
+				$phpDocTag->value->value,
+				$phpDocTag->value->exception->getMessage(),
+			))
+				->line(PhpDocLineHelper::detectLine($node, $phpDocTag))
+				->identifier('phpDoc.parseError')->build();
+		}
+
+		return $errors;
+	}
+
+}
