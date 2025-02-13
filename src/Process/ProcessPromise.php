c1320a204e9dee85f26a7ea7095f8b83935ddfb9
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 20:37:46 +0000

    deploy: 1.12.15

---
+++ src/Process/ProcessPromise.php
@@ -0,0 +1,99 @@
+<?php 
+
+namespace PHPStan\Process;
+return;
+
+use PHPStan\ShouldNotHappenException;
+use React\ChildProcess\Process;
+use React\EventLoop\LoopInterface;
+use React\Promise\Deferred;
+use React\Promise\PromiseInterface;
+use function fclose;
+use function rewind;
+use function stream_get_contents;
+use function tmpfile;
+
+final class ProcessPromise
+{
+
+	/** @var Deferred<string> */
+	private Deferred $deferred;
+
+	private ?Process $process = null;
+
+	private bool $canceled = false;
+
+	public function __construct(private LoopInterface $loop, private string $name, private string $command)
+	{
+		$this->deferred = new Deferred();
+	}
+
+	public function getName(): string
+	{
+		return $this->name;
+	}
+
+	/**
+	 * @return PromiseInterface<string>
+	 */
+	public function run(): PromiseInterface
+	{
+		$tmpStdOutResource = tmpfile();
+		if ($tmpStdOutResource === false) {
+			throw new ShouldNotHappenException('Failed creating temp file for stdout.');
+		}
+		$tmpStdErrResource = tmpfile();
+		if ($tmpStdErrResource === false) {
+			throw new ShouldNotHappenException('Failed creating temp file for stderr.');
+		}
+
+		$this->process = new Process($this->command, null, null, [
+			1 => $tmpStdOutResource,
+			2 => $tmpStdErrResource,
+		]);
+		$this->process->start($this->loop);
+
+		$this->process->on('exit', function ($exitCode) use ($tmpStdOutResource, $tmpStdErrResource): void {
+			if ($this->canceled) {
+				fclose($tmpStdOutResource);
+				fclose($tmpStdErrResource);
+				return;
+			}
+			rewind($tmpStdOutResource);
+			$stdOut = stream_get_contents($tmpStdOutResource);
+			fclose($tmpStdOutResource);
+
+			rewind($tmpStdErrResource);
+			$stdErr = stream_get_contents($tmpStdErrResource);
+			fclose($tmpStdErrResource);
+
+			if ($exitCode === null) {
+				$this->deferred->reject(new ProcessCrashedException($stdOut . $stdErr));
+				return;
+			}
+
+			if ($exitCode === 0) {
+				if ($stdOut === false) {
+					$stdOut = '';
+				}
+				$this->deferred->resolve($stdOut);
+				return;
+			}
+
+			$this->deferred->reject(new ProcessCrashedException($stdOut . $stdErr));
+		});
+
+		return $this->deferred->promise();
+	}
+
+	public function cancel(): void
+	{
+		if ($this->process === null) {
+			throw new ShouldNotHappenException('Cancelling process before running');
+		}
+		$this->canceled = true;
+		$this->process->terminate();
+		$this->deferred->reject(new ProcessCanceledException());
+	}
+
+}
---
+++ src/Reflection/Dummy/ChangedTypeMethodReflection.php
@@ -0,0 +1,164 @@
+<?php 
+
+namespace PHPStan\Reflection\Dummy;
+return;
+
+use PHPStan\Reflection\Assertions;
+use PHPStan\Reflection\ClassMemberReflection;
+use PHPStan\Reflection\ClassReflection;
+use PHPStan\Reflection\ExtendedMethodReflection;
+use PHPStan\Reflection\ExtendedParametersAcceptor;
+use PHPStan\ShouldNotHappenException;
+use PHPStan\TrinaryLogic;
+use PHPStan\Type\Type;
+use function count;
+use function is_bool;
+
+final class ChangedTypeMethodReflection implements ExtendedMethodReflection
+{
+
+	/**
+	 * @param list<ExtendedParametersAcceptor> $variants
+	 * @param list<ExtendedParametersAcceptor>|null $namedArgumentsVariants
+	 */
+	public function __construct(
+		private ClassReflection $declaringClass,
+		private ExtendedMethodReflection $reflection,
+		private array $variants,
+		private ?array $namedArgumentsVariants,
+		private ?Type $selfOutType,
+	)
+	{
+	}
+
+	public function getDeclaringClass(): ClassReflection
+	{
+		return $this->declaringClass;
+	}
+
+	public function isStatic(): bool
+	{
+		return $this->reflection->isStatic();
+	}
+
+	public function isPrivate(): bool
+	{
+		return $this->reflection->isPrivate();
+	}
+
+	public function isPublic(): bool
+	{
+		return $this->reflection->isPublic();
+	}
+
+	public function getDocComment(): ?string
+	{
+		return $this->reflection->getDocComment();
+	}
+
+	public function getName(): string
+	{
+		return $this->reflection->getName();
+	}
+
+	public function getPrototype(): ClassMemberReflection
+	{
+		return $this->reflection->getPrototype();
+	}
+
+	public function getVariants(): array
+	{
+		return $this->variants;
+	}
+
+	public function getOnlyVariant(): ExtendedParametersAcceptor
+	{
+		$variants = $this->getVariants();
+		if (count($variants) !== 1) {
+			throw new ShouldNotHappenException();
+		}
+
+		return $variants[0];
+	}
+
+	public function getNamedArgumentsVariants(): ?array
+	{
+		return $this->namedArgumentsVariants;
+	}
+
+	public function isDeprecated(): TrinaryLogic
+	{
+		return $this->reflection->isDeprecated();
+	}
+
+	public function getDeprecatedDescription(): ?string
+	{
+		return $this->reflection->getDeprecatedDescription();
+	}
+
+	public function isFinal(): TrinaryLogic
+	{
+		return $this->reflection->isFinal();
+	}
+
+	public function isFinalByKeyword(): TrinaryLogic
+	{
+		return $this->reflection->isFinalByKeyword();
+	}
+
+	public function isInternal(): TrinaryLogic
+	{
+		return $this->reflection->isInternal();
+	}
+
+	public function getThrowType(): ?Type
+	{
+		return $this->reflection->getThrowType();
+	}
+
+	public function hasSideEffects(): TrinaryLogic
+	{
+		return $this->reflection->hasSideEffects();
+	}
+
+	public function getAsserts(): Assertions
+	{
+		return $this->reflection->getAsserts();
+	}
+
+	public function acceptsNamedArguments(): TrinaryLogic
+	{
+		return $this->reflection->acceptsNamedArguments();
+	}
+
+	public function getSelfOutType(): ?Type
+	{
+		return $this->selfOutType;
+	}
+
+	public function returnsByReference(): TrinaryLogic
+	{
+		return $this->reflection->returnsByReference();
+	}
+
+	public function isAbstract(): TrinaryLogic
+	{
+		$abstract = $this->reflection->isAbstract();
+		if (is_bool($abstract)) {
+			return TrinaryLogic::createFromBoolean($abstract);
+		}
+
+		return $abstract;
+	}
+
+	public function isPure(): TrinaryLogic
+	{
+		return $this->reflection->isPure();
+	}
+
+	public function getAttributes(): array
+	{
+		return $this->reflection->getAttributes();
+	}
+
+}
