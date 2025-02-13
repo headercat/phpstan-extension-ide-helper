8fc9c4b4e33cce55db51b2f365d3d8b755d40965
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 20:44:49 +0000

    deploy: 1.12.5

---
+++ src/Reflection/BetterReflection/SourceLocator/OptimizedSingleFileSourceLocatorFactory.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace PHPStan\Reflection\BetterReflection\SourceLocator;
+return;
+
+interface OptimizedSingleFileSourceLocatorFactory
+{
+
+	public function create(string $fileName): OptimizedSingleFileSourceLocator;
+
+}
