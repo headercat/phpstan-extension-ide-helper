433255650425f8161f83fcedafc7d17d6afc8ff3
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 20:08:46 +0000

    deploy: 1.10.61

---
+++ src/Parser/ClosureBindToVarVisitor.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace PHPStan\Parser;
+return;
+
+use PhpParser\Node;
+use PhpParser\Node\Identifier;
+use PhpParser\NodeVisitorAbstract;
+
+final class ClosureBindToVarVisitor extends NodeVisitorAbstract
+{
+
+	public const ATTRIBUTE_NAME = 'closureBindToVar';
+
+	public function enterNode(Node $node): ?Node
+	{
+		if (
+			$node instanceof Node\Expr\MethodCall
+			&& $node->name instanceof Identifier
+			&& $node->name->toLowerString() === 'bindto'
+			&& !$node->isFirstClassCallable()
+		) {
+			$args = $node->getArgs();
+			if (isset($args[0])) {
+				$args[0]->setAttribute(self::ATTRIBUTE_NAME, $node->var);
+			}
+		}
+		return null;
+	}
+
+}
