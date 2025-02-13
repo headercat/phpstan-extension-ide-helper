2cfb4915901506f616e2b3ad002f32b9d4875ceb
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 20:33:45 +0000

    deploy: 1.12.11

---
+++ src/Rules/Functions/ReturnTypeRule.php
@@ -0,0 +1,71 @@
+<?php 
+
+namespace PHPStan\Rules\Functions;
+return;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Return_;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Rules\FunctionReturnTypeCheck;
+use PHPStan\Rules\Rule;
+use function sprintf;
+
+/**
+ * @implements Rule<Node\Stmt\Return_>
+ */
+final class ReturnTypeRule implements Rule
+{
+
+	public function __construct(
+		private FunctionReturnTypeCheck $returnTypeCheck,
+	)
+	{
+	}
+
+	public function getNodeType(): string
+	{
+		return Return_::class;
+	}
+
+	public function processNode(Node $node, Scope $scope): array
+	{
+		if ($scope->getFunction() === null) {
+			return [];
+		}
+
+		if ($scope->isInAnonymousFunction()) {
+			return [];
+		}
+
+		$function = $scope->getFunction();
+		if ($function instanceof MethodReflection) {
+			return [];
+		}
+
+		return $this->returnTypeCheck->checkReturnType(
+			$scope,
+			$function->getReturnType(),
+			$node->expr,
+			$node,
+			sprintf(
+				'Function %s() should return %%s but empty return statement found.',
+				$function->getName(),
+			),
+			sprintf(
+				'Function %s() with return type void returns %%s but should not return anything.',
+				$function->getName(),
+			),
+			sprintf(
+				'Function %s() should return %%s but returns %%s.',
+				$function->getName(),
+			),
+			sprintf(
+				'Function %s() should never return but return statement found.',
+				$function->getName(),
+			),
+			$function->isGenerator(),
+		);
+	}
+
+}
