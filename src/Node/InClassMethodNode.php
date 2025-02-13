319a918cce352ee8804c737b5f65c50dae92abab
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 22:06:57 +0000

    deploy: 1.8.7

---
+++ src/Node/InClassMethodNode.php
@@ -0,0 +1,53 @@
+<?php 
+
+namespace PHPStan\Node;
+return;
+
+use PhpParser\Node;
+use PHPStan\Reflection\ClassReflection;
+use PHPStan\Reflection\Php\PhpMethodFromParserNodeReflection;
+
+/**
+ * @api
+ */
+final class InClassMethodNode extends Node\Stmt implements VirtualNode
+{
+
+	public function __construct(
+		private ClassReflection $classReflection,
+		private PhpMethodFromParserNodeReflection $methodReflection,
+		private Node\Stmt\ClassMethod $originalNode,
+	)
+	{
+		parent::__construct($originalNode->getAttributes());
+	}
+
+	public function getClassReflection(): ClassReflection
+	{
+		return $this->classReflection;
+	}
+
+	public function getMethodReflection(): PhpMethodFromParserNodeReflection
+	{
+		return $this->methodReflection;
+	}
+
+	public function getOriginalNode(): Node\Stmt\ClassMethod
+	{
+		return $this->originalNode;
+	}
+
+	public function getType(): string
+	{
+		return 'PHPStan_Stmt_InClassMethodNode';
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public function getSubNodeNames(): array
+	{
+		return [];
+	}
+
+}
---
+++ src/Rules/Properties/PropertyAttributesRule.php
@@ -0,0 +1,37 @@
+<?php 
+
+namespace PHPStan\Rules\Properties;
+return;
+
+use Attribute;
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\AttributesCheck;
+use PHPStan\Rules\Rule;
+
+/**
+ * @implements Rule<Node\Stmt\Property>
+ */
+final class PropertyAttributesRule implements Rule
+{
+
+	public function __construct(private AttributesCheck $attributesCheck)
+	{
+	}
+
+	public function getNodeType(): string
+	{
+		return Node\Stmt\Property::class;
+	}
+
+	public function processNode(Node $node, Scope $scope): array
+	{
+		return $this->attributesCheck->check(
+			$scope,
+			$node->attrGroups,
+			Attribute::TARGET_PROPERTY,
+			'property',
+		);
+	}
+
+}
