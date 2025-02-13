48b125ffab65a042497b14ec4797f40fb852c1d1
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 21:00:49 +0000

    deploy: 1.4.5

---
+++ src/Reflection/Type/IntersectionTypeUnresolvedPropertyPrototypeReflection.php
@@ -0,0 +1,57 @@
+<?php 
+
+namespace PHPStan\Reflection\Type;
+return;
+
+use PHPStan\Reflection\ExtendedPropertyReflection;
+use PHPStan\Reflection\PropertyReflection;
+use PHPStan\Type\Type;
+use function array_map;
+
+final class IntersectionTypeUnresolvedPropertyPrototypeReflection implements UnresolvedPropertyPrototypeReflection
+{
+
+	private ?ExtendedPropertyReflection $transformedProperty = null;
+
+	private ?self $cachedDoNotResolveTemplateTypeMapToBounds = null;
+
+	/**
+	 * @param UnresolvedPropertyPrototypeReflection[] $propertyPrototypes
+	 */
+	public function __construct(
+		private string $propertyName,
+		private array $propertyPrototypes,
+	)
+	{
+	}
+
+	public function doNotResolveTemplateTypeMapToBounds(): UnresolvedPropertyPrototypeReflection
+	{
+		if ($this->cachedDoNotResolveTemplateTypeMapToBounds !== null) {
+			return $this->cachedDoNotResolveTemplateTypeMapToBounds;
+		}
+
+		return $this->cachedDoNotResolveTemplateTypeMapToBounds = new self($this->propertyName, array_map(static fn (UnresolvedPropertyPrototypeReflection $prototype): UnresolvedPropertyPrototypeReflection => $prototype->doNotResolveTemplateTypeMapToBounds(), $this->propertyPrototypes));
+	}
+
+	public function getNakedProperty(): ExtendedPropertyReflection
+	{
+		return $this->getTransformedProperty();
+	}
+
+	public function getTransformedProperty(): ExtendedPropertyReflection
+	{
+		if ($this->transformedProperty !== null) {
+			return $this->transformedProperty;
+		}
+		$properties = array_map(static fn (UnresolvedPropertyPrototypeReflection $prototype): PropertyReflection => $prototype->getTransformedProperty(), $this->propertyPrototypes);
+
+		return $this->transformedProperty = new IntersectionTypePropertyReflection($properties);
+	}
+
+	public function withFechedOnType(Type $type): UnresolvedPropertyPrototypeReflection
+	{
+		return new self($this->propertyName, array_map(static fn (UnresolvedPropertyPrototypeReflection $prototype): UnresolvedPropertyPrototypeReflection => $prototype->withFechedOnType($type), $this->propertyPrototypes));
+	}
+
+}
---
+++ src/Rules/PhpDoc/UnresolvableTypeHelper.php
@@ -0,0 +1,33 @@
+<?php 
+
+namespace PHPStan\Rules\PhpDoc;
+return;
+
+use PHPStan\Type\ErrorType;
+use PHPStan\Type\NeverType;
+use PHPStan\Type\Type;
+use PHPStan\Type\TypeTraverser;
+
+final class UnresolvableTypeHelper
+{
+
+	public function containsUnresolvableType(Type $type): bool
+	{
+		$containsUnresolvable = false;
+		TypeTraverser::map($type, static function (Type $type, callable $traverse) use (&$containsUnresolvable): Type {
+			if ($type instanceof ErrorType) {
+				$containsUnresolvable = true;
+				return $type;
+			}
+			if ($type instanceof NeverType && !$type->isExplicit()) {
+				$containsUnresolvable = true;
+				return $type;
+			}
+
+			return $traverse($type);
+		});
+
+		return $containsUnresolvable;
+	}
+
+}
