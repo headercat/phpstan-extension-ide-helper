8a3f6b428e3bef67bbe314d7426a9f3a6c62b715
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 20:32:43 +0000

    deploy: 1.12.10

---
+++ src/PhpDoc/DefaultStubFilesProvider.php
@@ -0,0 +1,75 @@
+<?php 
+
+namespace PHPStan\PhpDoc;
+return;
+
+use PHPStan\DependencyInjection\Container;
+use PHPStan\Internal\ComposerHelper;
+use function array_filter;
+use function array_values;
+use function str_contains;
+use function strtr;
+
+final class DefaultStubFilesProvider implements StubFilesProvider
+{
+
+	/** @var string[]|null */
+	private ?array $cachedFiles = null;
+
+	/** @var string[]|null */
+	private ?array $cachedProjectFiles = null;
+
+	/**
+	 * @param string[] $stubFiles
+	 * @param string[] $composerAutoloaderProjectPaths
+	 */
+	public function __construct(
+		private Container $container,
+		private array $stubFiles,
+		private array $composerAutoloaderProjectPaths,
+	)
+	{
+	}
+
+	public function getStubFiles(): array
+	{
+		if ($this->cachedFiles !== null) {
+			return $this->cachedFiles;
+		}
+
+		$files = $this->stubFiles;
+		$extensions = $this->container->getServicesByTag(StubFilesExtension::EXTENSION_TAG);
+		foreach ($extensions as $extension) {
+			foreach ($extension->getFiles() as $extensionFile) {
+				$files[] = $extensionFile;
+			}
+		}
+
+		return $this->cachedFiles = $files;
+	}
+
+	public function getProjectStubFiles(): array
+	{
+		if ($this->cachedProjectFiles !== null) {
+			return $this->cachedProjectFiles;
+		}
+
+		$filteredStubFiles = $this->getStubFiles();
+		foreach ($this->composerAutoloaderProjectPaths as $composerAutoloaderProjectPath) {
+			$composerConfig = ComposerHelper::getComposerConfig($composerAutoloaderProjectPath);
+			if ($composerConfig === null) {
+				continue;
+			}
+
+			$vendorDir = ComposerHelper::getVendorDirFromComposerConfig($composerAutoloaderProjectPath, $composerConfig);
+			$vendorDir = strtr($vendorDir, '\\', '/');
+			$filteredStubFiles = array_filter(
+				$filteredStubFiles,
+				static fn (string $file): bool => !str_contains(strtr($file, '\\', '/'), $vendorDir)
+			);
+		}
+
+		return $this->cachedProjectFiles = array_values($filteredStubFiles);
+	}
+
+}
