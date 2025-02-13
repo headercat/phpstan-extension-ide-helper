5b3914b026429996c85dbde94cede1febc7f4ab7
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 21:42:58 +0000

    deploy: 1.7.1

---
+++ src/Type/BenevolentUnionType.php
@@ -0,0 +1,182 @@
+<?php 
+
+namespace PHPStan\Type;
+return;
+
+use PHPStan\TrinaryLogic;
+use PHPStan\Type\Generic\TemplateTypeMap;
+use function count;
+
+/** @api */
+class BenevolentUnionType extends UnionType
+{
+
+	/**
+	 * @api
+	 * @param Type[] $types
+	 */
+	public function __construct(array $types, bool $normalized = false)
+	{
+		parent::__construct($types, $normalized);
+	}
+
+	public function filterTypes(callable $filterCb): Type
+	{
+		$result = parent::filterTypes($filterCb);
+		if (!$result instanceof self && $result instanceof UnionType) {
+			return TypeUtils::toBenevolentUnion($result);
+		}
+
+		return $result;
+	}
+
+	public function describe(VerbosityLevel $level): string
+	{
+		return '(' . parent::describe($level) . ')';
+	}
+
+	protected function unionTypes(callable $getType): Type
+	{
+		$resultTypes = [];
+		foreach ($this->getTypes() as $type) {
+			$result = $getType($type);
+			if ($result instanceof ErrorType) {
+				continue;
+			}
+
+			$resultTypes[] = $result;
+		}
+
+		if (count($resultTypes) === 0) {
+			return new ErrorType();
+		}
+
+		return TypeUtils::toBenevolentUnion(TypeCombinator::union(...$resultTypes));
+	}
+
+	protected function pickFromTypes(
+		callable $getValues,
+		callable $criteria,
+	): array
+	{
+		$values = [];
+		foreach ($this->getTypes() as $type) {
+			$innerValues = $getValues($type);
+			if ($innerValues === [] && $criteria($type)) {
+				return [];
+			}
+
+			foreach ($innerValues as $innerType) {
+				$values[] = $innerType;
+			}
+		}
+
+		return $values;
+	}
+
+	public function getOffsetValueType(Type $offsetType): Type
+	{
+		$types = [];
+		foreach ($this->getTypes() as $innerType) {
+			$valueType = $innerType->getOffsetValueType($offsetType);
+			if ($valueType instanceof ErrorType) {
+				continue;
+			}
+
+			$types[] = $valueType;
+		}
+
+		if (count($types) === 0) {
+			return new ErrorType();
+		}
+
+		return TypeUtils::toBenevolentUnion(TypeCombinator::union(...$types));
+	}
+
+	protected function unionResults(callable $getResult): TrinaryLogic
+	{
+		return TrinaryLogic::createNo()->lazyOr($this->getTypes(), $getResult);
+	}
+
+	public function isAcceptedBy(Type $acceptingType, bool $strictTypes): AcceptsResult
+	{
+		$result = AcceptsResult::createNo();
+		foreach ($this->getTypes() as $innerType) {
+			$result = $result->or($acceptingType->accepts($innerType, $strictTypes));
+		}
+
+		return $result;
+	}
+
+	public function inferTemplateTypes(Type $receivedType): TemplateTypeMap
+	{
+		$types = TemplateTypeMap::createEmpty();
+
+		foreach ($this->getTypes() as $type) {
+			$types = $types->benevolentUnion($type->inferTemplateTypes($receivedType));
+		}
+
+		return $types;
+	}
+
+	public function inferTemplateTypesOn(Type $templateType): TemplateTypeMap
+	{
+		$types = TemplateTypeMap::createEmpty();
+
+		foreach ($this->getTypes() as $type) {
+			$types = $types->benevolentUnion($templateType->inferTemplateTypes($type));
+		}
+
+		return $types;
+	}
+
+	public function traverse(callable $cb): Type
+	{
+		$types = [];
+		$changed = false;
+
+		foreach ($this->getTypes() as $type) {
+			$newType = $cb($type);
+			if ($type !== $newType) {
+				$changed = true;
+			}
+			$types[] = $newType;
+		}
+
+		if ($changed) {
+			return TypeUtils::toBenevolentUnion(TypeCombinator::union(...$types));
+		}
+
+		return $this;
+	}
+
+	public function traverseSimultaneously(Type $right, callable $cb): Type
+	{
+		$types = [];
+		$changed = false;
+
+		if (!$right instanceof UnionType) {
+			return $this;
+		}
+
+		if (count($this->getTypes()) !== count($right->getTypes())) {
+			return $this;
+		}
+
+		foreach ($this->getSortedTypes() as $i => $leftType) {
+			$rightType = $right->getSortedTypes()[$i];
+			$newType = $cb($leftType, $rightType);
+			if ($leftType !== $newType) {
+				$changed = true;
+			}
+			$types[] = $newType;
+		}
+
+		if ($changed) {
+			return TypeUtils::toBenevolentUnion(TypeCombinator::union(...$types));
+		}
+
+		return $this;
+	}
+
+}
---
+++ src/Type/ErrorType.php
@@ -0,0 +1,45 @@
+<?php 
+
+namespace PHPStan\Type;
+return;
+
+/** @api */
+class ErrorType extends MixedType
+{
+
+	/** @api */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function describe(VerbosityLevel $level): string
+	{
+		return $level->handle(
+			fn (): string => parent::describe($level),
+			fn (): string => parent::describe($level),
+			static fn (): string => '*ERROR*',
+		);
+	}
+
+	public function getIterableKeyType(): Type
+	{
+		return new ErrorType();
+	}
+
+	public function getIterableValueType(): Type
+	{
+		return new ErrorType();
+	}
+
+	public function subtract(Type $type): Type
+	{
+		return new self();
+	}
+
+	public function equals(Type $type): bool
+	{
+		return $type instanceof self;
+	}
+
+}
