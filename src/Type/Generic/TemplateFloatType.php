a5b1edd6aa52ec990580dbfc3ca1509721e200b6
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 21:30:08 +0000

    deploy: 1.5.6

---
+++ src/Type/Generic/TemplateFloatType.php
@@ -0,0 +1,44 @@
+<?php 
+
+namespace PHPStan\Type\Generic;
+return;
+
+use PHPStan\Type\FloatType;
+use PHPStan\Type\Traits\UndecidedComparisonCompoundTypeTrait;
+use PHPStan\Type\Type;
+
+/** @api */
+final class TemplateFloatType extends FloatType implements TemplateType
+{
+
+	/** @use TemplateTypeTrait<FloatType> */
+	use TemplateTypeTrait;
+	use UndecidedComparisonCompoundTypeTrait;
+
+	/**
+	 * @param non-empty-string $name
+	 */
+	public function __construct(
+		TemplateTypeScope $scope,
+		TemplateTypeStrategy $templateTypeStrategy,
+		TemplateTypeVariance $templateTypeVariance,
+		string $name,
+		FloatType $bound,
+		?Type $default,
+	)
+	{
+		parent::__construct();
+		$this->scope = $scope;
+		$this->strategy = $templateTypeStrategy;
+		$this->variance = $templateTypeVariance;
+		$this->name = $name;
+		$this->bound = $bound;
+		$this->default = $default;
+	}
+
+	protected function shouldGeneralizeInferredType(): bool
+	{
+		return false;
+	}
+
+}
---
+++ src/Type/Php/DateIntervalConstructorThrowTypeExtension.php
@@ -0,0 +1,65 @@
+<?php 
+
+namespace PHPStan\Type\Php;
+return;
+
+use DateInterval;
+use PhpParser\Node\Expr\StaticCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Php\PhpVersion;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\DynamicStaticMethodThrowTypeExtension;
+use PHPStan\Type\NeverType;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\Type;
+use PHPStan\Type\TypeCombinator;
+use function count;
+
+final class DateIntervalConstructorThrowTypeExtension implements DynamicStaticMethodThrowTypeExtension
+{
+
+	public function __construct(private PhpVersion $phpVersion)
+	{
+	}
+
+	public function isStaticMethodSupported(MethodReflection $methodReflection): bool
+	{
+		return $methodReflection->getName() === '__construct' && $methodReflection->getDeclaringClass()->getName() === DateInterval::class;
+	}
+
+	public function getThrowTypeFromStaticMethodCall(MethodReflection $methodReflection, StaticCall $methodCall, Scope $scope): ?Type
+	{
+		if (count($methodCall->getArgs()) === 0) {
+			return $methodReflection->getThrowType();
+		}
+
+		$valueType = $scope->getType($methodCall->getArgs()[0]->value);
+		$constantStrings = $valueType->getConstantStrings();
+
+		foreach ($constantStrings as $constantString) {
+			try {
+				new DateInterval($constantString->getValue());
+			} catch (\Exception $e) { // phpcs:ignore
+				return $this->exceptionType();
+			}
+
+			$valueType = TypeCombinator::remove($valueType, $constantString);
+		}
+
+		if (!$valueType instanceof NeverType) {
+			return $this->exceptionType();
+		}
+
+		return null;
+	}
+
+	private function exceptionType(): Type
+	{
+		if ($this->phpVersion->hasDateTimeExceptions()) {
+			return new ObjectType('DateMalformedIntervalStringException');
+		}
+
+		return new ObjectType('Exception');
+	}
+
+}
