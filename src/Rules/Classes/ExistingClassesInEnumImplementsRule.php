87feaf6556274fe11037e33379095b874c0c8a10
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 20:49:43 +0000

    deploy: 1.2.0

---
+++ src/Rules/Classes/ExistingClassesInEnumImplementsRule.php
@@ -0,0 +1,92 @@
+<?php 
+
+namespace PHPStan\Rules\Classes;
+return;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\ReflectionProvider;
+use PHPStan\Rules\ClassNameCheck;
+use PHPStan\Rules\ClassNameNodePair;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
+use function array_map;
+use function sprintf;
+
+/**
+ * @implements Rule<Node\Stmt\Enum_>
+ */
+final class ExistingClassesInEnumImplementsRule implements Rule
+{
+
+	public function __construct(
+		private ClassNameCheck $classCheck,
+		private ReflectionProvider $reflectionProvider,
+	)
+	{
+	}
+
+	public function getNodeType(): string
+	{
+		return Node\Stmt\Enum_::class;
+	}
+
+	public function processNode(Node $node, Scope $scope): array
+	{
+		$messages = $this->classCheck->checkClassNames(
+			array_map(static fn (Node\Name $interfaceName): ClassNameNodePair => new ClassNameNodePair((string) $interfaceName, $interfaceName), $node->implements),
+		);
+
+		$currentEnumName = (string) $node->namespacedName;
+
+		foreach ($node->implements as $implements) {
+			$implementedClassName = (string) $implements;
+			if (!$this->reflectionProvider->hasClass($implementedClassName)) {
+				if (!$scope->isInClassExists($implementedClassName)) {
+					$messages[] = RuleErrorBuilder::message(sprintf(
+						'Enum %s implements unknown interface %s.',
+						$currentEnumName,
+						$implementedClassName,
+					))
+						->identifier('interface.notFound')
+						->nonIgnorable()
+						->discoveringSymbolsTip()
+						->build();
+				}
+			} else {
+				$reflection = $this->reflectionProvider->getClass($implementedClassName);
+				if ($reflection->isClass()) {
+					$messages[] = RuleErrorBuilder::message(sprintf(
+						'Enum %s implements class %s.',
+						$currentEnumName,
+						$reflection->getDisplayName(),
+					))
+						->identifier('enumImplements.class')
+						->nonIgnorable()
+						->build();
+				} elseif ($reflection->isTrait()) {
+					$messages[] = RuleErrorBuilder::message(sprintf(
+						'Enum %s implements trait %s.',
+						$currentEnumName,
+						$reflection->getDisplayName(),
+					))
+						->identifier('enumImplements.trait')
+						->nonIgnorable()
+						->build();
+				} elseif ($reflection->isEnum()) {
+					$messages[] = RuleErrorBuilder::message(sprintf(
+						'Enum %s implements enum %s.',
+						$currentEnumName,
+						$reflection->getDisplayName(),
+					))
+						->identifier('enumImplements.enum')
+						->nonIgnorable()
+						->build();
+				}
+			}
+		}
+
+		return $messages;
+	}
+
+}
---
+++ src/Type/ValueOfType.php
@@ -0,0 +1,104 @@
+<?php 
+
+namespace PHPStan\Type;
+return;
+
+use PHPStan\PhpDocParser\Ast\Type\GenericTypeNode;
+use PHPStan\PhpDocParser\Ast\Type\IdentifierTypeNode;
+use PHPStan\PhpDocParser\Ast\Type\TypeNode;
+use PHPStan\Type\Generic\TemplateTypeVariance;
+use PHPStan\Type\Traits\LateResolvableTypeTrait;
+use PHPStan\Type\Traits\NonGeneralizableTypeTrait;
+use function sprintf;
+
+/** @api */
+final class ValueOfType implements CompoundType, LateResolvableType
+{
+
+	use LateResolvableTypeTrait;
+	use NonGeneralizableTypeTrait;
+
+	public function __construct(private Type $type)
+	{
+	}
+
+	public function getReferencedClasses(): array
+	{
+		return $this->type->getReferencedClasses();
+	}
+
+	public function getReferencedTemplateTypes(TemplateTypeVariance $positionVariance): array
+	{
+		return $this->type->getReferencedTemplateTypes($positionVariance);
+	}
+
+	public function equals(Type $type): bool
+	{
+		return $type instanceof self
+			&& $this->type->equals($type->type);
+	}
+
+	public function describe(VerbosityLevel $level): string
+	{
+		return sprintf('value-of<%s>', $this->type->describe($level));
+	}
+
+	public function isResolvable(): bool
+	{
+		return !TypeUtils::containsTemplateType($this->type);
+	}
+
+	protected function getResult(): Type
+	{
+		if ($this->type->isEnum()->yes()) {
+			$valueTypes = [];
+			foreach ($this->type->getEnumCases() as $enumCase) {
+				$valueType = $enumCase->getBackingValueType();
+				if ($valueType === null) {
+					continue;
+				}
+
+				$valueTypes[] = $valueType;
+			}
+
+			return TypeCombinator::union(...$valueTypes);
+		}
+
+		return $this->type->getIterableValueType();
+	}
+
+	/**
+	 * @param callable(Type): Type $cb
+	 */
+	public function traverse(callable $cb): Type
+	{
+		$type = $cb($this->type);
+
+		if ($this->type === $type) {
+			return $this;
+		}
+
+		return new self($type);
+	}
+
+	public function traverseSimultaneously(Type $right, callable $cb): Type
+	{
+		if (!$right instanceof self) {
+			return $this;
+		}
+
+		$type = $cb($this->type, $right->type);
+
+		if ($this->type === $type) {
+			return $this;
+		}
+
+		return new self($type);
+	}
+
+	public function toPhpDocNode(): TypeNode
+	{
+		return new GenericTypeNode(new IdentifierTypeNode('value-of'), [$this->type->toPhpDocNode()]);
+	}
+
+}
