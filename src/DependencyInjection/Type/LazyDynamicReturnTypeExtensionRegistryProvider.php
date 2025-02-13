b6933d3b91330dd080dc6fdc4b800a81d33a62f6
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 19:57:44 +0000

    deploy: 1.10.51

---
+++ src/DependencyInjection/Type/LazyDynamicReturnTypeExtensionRegistryProvider.php
@@ -0,0 +1,34 @@
+<?php 
+
+namespace PHPStan\DependencyInjection\Type;
+return;
+
+use PHPStan\Broker\BrokerFactory;
+use PHPStan\DependencyInjection\Container;
+use PHPStan\Reflection\ReflectionProvider;
+use PHPStan\Type\DynamicReturnTypeExtensionRegistry;
+
+final class LazyDynamicReturnTypeExtensionRegistryProvider implements DynamicReturnTypeExtensionRegistryProvider
+{
+
+	private ?DynamicReturnTypeExtensionRegistry $registry = null;
+
+	public function __construct(private Container $container)
+	{
+	}
+
+	public function getRegistry(): DynamicReturnTypeExtensionRegistry
+	{
+		if ($this->registry === null) {
+			$this->registry = new DynamicReturnTypeExtensionRegistry(
+				$this->container->getByType(ReflectionProvider::class),
+				$this->container->getServicesByTag(BrokerFactory::DYNAMIC_METHOD_RETURN_TYPE_EXTENSION_TAG),
+				$this->container->getServicesByTag(BrokerFactory::DYNAMIC_STATIC_METHOD_RETURN_TYPE_EXTENSION_TAG),
+				$this->container->getServicesByTag(BrokerFactory::DYNAMIC_FUNCTION_RETURN_TYPE_EXTENSION_TAG),
+			);
+		}
+
+		return $this->registry;
+	}
+
+}
---
+++ src/Node/InstantiationCallableNode.php
@@ -0,0 +1,46 @@
+<?php 
+
+namespace PHPStan\Node;
+return;
+
+use PhpParser\Node\Expr;
+use PhpParser\Node\Name;
+
+/**
+ * @api
+ */
+final class InstantiationCallableNode extends Expr implements VirtualNode
+{
+
+	public function __construct(private Name|Expr $class, private Expr\New_ $originalNode)
+	{
+		parent::__construct($this->originalNode->getAttributes());
+	}
+
+	/**
+	 * @return Expr|Name
+	 */
+	public function getClass()
+	{
+		return $this->class;
+	}
+
+	public function getOriginalNode(): Expr\New_
+	{
+		return $this->originalNode;
+	}
+
+	public function getType(): string
+	{
+		return 'PHPStan_Node_InstantiationCallableNode';
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public function getSubNodeNames(): array
+	{
+		return [];
+	}
+
+}
