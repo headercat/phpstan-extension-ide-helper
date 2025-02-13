a6caf5f58685b0ab582646dd9c926fc796bb3712
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 20:27:46 +0000

    deploy: 1.11.7

---
+++ src/Reflection/Php/PhpMethodReflectionFactory.php
@@ -0,0 +1,47 @@
+<?php 
+
+namespace PHPStan\Reflection\Php;
+return;
+
+use PHPStan\BetterReflection\Reflection\Adapter\ReflectionMethod;
+use PHPStan\Reflection\Assertions;
+use PHPStan\Reflection\AttributeReflection;
+use PHPStan\Reflection\ClassReflection;
+use PHPStan\TrinaryLogic;
+use PHPStan\Type\Generic\TemplateTypeMap;
+use PHPStan\Type\Type;
+
+interface PhpMethodReflectionFactory
+{
+
+	/**
+	 * @param Type[] $phpDocParameterTypes
+	 * @param Type[] $phpDocParameterOutTypes
+	 * @param array<string, TrinaryLogic> $immediatelyInvokedCallableParameters
+	 * @param array<string, Type> $phpDocClosureThisTypeParameters
+	 * @param list<AttributeReflection> $attributes
+	 */
+	public function create(
+		ClassReflection $declaringClass,
+		?ClassReflection $declaringTrait,
+		ReflectionMethod $reflection,
+		TemplateTypeMap $templateTypeMap,
+		array $phpDocParameterTypes,
+		?Type $phpDocReturnType,
+		?Type $phpDocThrowType,
+		?string $deprecatedDescription,
+		bool $isDeprecated,
+		bool $isInternal,
+		bool $isFinal,
+		?bool $isPure,
+		Assertions $asserts,
+		?Type $selfOutType,
+		?string $phpDocComment,
+		array $phpDocParameterOutTypes,
+		array $immediatelyInvokedCallableParameters,
+		array $phpDocClosureThisTypeParameters,
+		bool $acceptsNamedArguments,
+		array $attributes,
+	): PhpMethodReflection;
+
+}
---
+++ src/Type/TypeAliasResolverProvider.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace PHPStan\Type;
+return;
+
+interface TypeAliasResolverProvider
+{
+
+	public function getTypeAliasResolver(): TypeAliasResolver;
+
+}
