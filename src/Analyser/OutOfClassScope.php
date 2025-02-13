dab89578e558fc9240d63383a8d2c98ad8b56314
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 22:33:58 +0000

    deploy: 2.1.0

---
+++ src/Analyser/OutOfClassScope.php
@@ -0,0 +1,58 @@
+<?php 
+
+namespace PHPStan\Analyser;
+return;
+
+use PHPStan\Reflection\ClassConstantReflection;
+use PHPStan\Reflection\ClassMemberAccessAnswerer;
+use PHPStan\Reflection\ClassReflection;
+use PHPStan\Reflection\ExtendedPropertyReflection;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Reflection\PropertyReflection;
+
+final class OutOfClassScope implements ClassMemberAccessAnswerer
+{
+
+	/** @api */
+	public function __construct()
+	{
+	}
+
+	public function isInClass(): bool
+	{
+		return false;
+	}
+
+	public function getClassReflection(): ?ClassReflection
+	{
+		return null;
+	}
+
+	public function canAccessProperty(PropertyReflection $propertyReflection): bool
+	{
+		return $propertyReflection->isPublic();
+	}
+
+	public function canReadProperty(ExtendedPropertyReflection $propertyReflection): bool
+	{
+		return $propertyReflection->isPublic();
+	}
+
+	public function canWriteProperty(ExtendedPropertyReflection $propertyReflection): bool
+	{
+		return $propertyReflection->isPublic()
+			&& !$propertyReflection->isProtectedSet()
+			&& !$propertyReflection->isPrivateSet();
+	}
+
+	public function canCallMethod(MethodReflection $methodReflection): bool
+	{
+		return $methodReflection->isPublic();
+	}
+
+	public function canAccessConstant(ClassConstantReflection $constantReflection): bool
+	{
+		return $constantReflection->isPublic();
+	}
+
+}
