93585c8c7cbe0527906529f91eef061ac1912f10
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 21:24:01 +0000

    deploy: 1.5.0

---
+++ src/Node/Constant/ClassConstantFetch.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace PHPStan\Node\Constant;
+return;
+
+use PhpParser\Node\Expr\ClassConstFetch;
+use PHPStan\Analyser\Scope;
+
+/**
+ * @api
+ */
+final class ClassConstantFetch
+{
+
+	public function __construct(private ClassConstFetch $node, private Scope $scope)
+	{
+	}
+
+	public function getNode(): ClassConstFetch
+	{
+		return $this->node;
+	}
+
+	public function getScope(): Scope
+	{
+		return $this->scope;
+	}
+
+}
