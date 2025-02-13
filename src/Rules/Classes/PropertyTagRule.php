486a27ebda3e5397aee172229c3ccb699d5905a1
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 20:14:44 +0000

    deploy: 1.10.67

---
+++ src/Rules/Classes/PropertyTagRule.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace PHPStan\Rules\Classes;
+return;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Node\InClassNode;
+use PHPStan\Rules\Rule;
+
+/**
+ * @implements Rule<InClassNode>
+ */
+final class PropertyTagRule implements Rule
+{
+
+	public function __construct(private PropertyTagCheck $check)
+	{
+	}
+
+	public function getNodeType(): string
+	{
+		return InClassNode::class;
+	}
+
+	public function processNode(Node $node, Scope $scope): array
+	{
+		return $this->check->check($node->getClassReflection(), $node->getOriginalNode());
+	}
+
+}
