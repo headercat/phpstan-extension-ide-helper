d593077e425f60a31794a81dd0e5a7c9c150f2a9
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 19:09:18 +0000

    deploy: 1.10.15

---
+++ src/File/SimpleRelativePathHelper.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace PHPStan\File;
+return;
+
+use function str_replace;
+use function str_starts_with;
+use function strlen;
+use function substr;
+
+final class SimpleRelativePathHelper implements RelativePathHelper
+{
+
+	public function __construct(private string $currentWorkingDirectory)
+	{
+	}
+
+	public function getRelativePath(string $filename): string
+	{
+		if ($this->currentWorkingDirectory !== '' && str_starts_with($filename, $this->currentWorkingDirectory)) {
+			return str_replace('\\', '/', substr($filename, strlen($this->currentWorkingDirectory) + 1));
+		}
+
+		return str_replace('\\', '/', $filename);
+	}
+
+}
