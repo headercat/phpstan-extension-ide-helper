689104f160ad3a031e731ba5b8b2deca7ed6b630
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 21:48:56 +0000

    deploy: 1.7.15

---
+++ src/Reflection/Native/NativeParameterReflection.php
@@ -0,0 +1,67 @@
+<?php 
+
+namespace PHPStan\Reflection\Native;
+return;
+
+use PHPStan\Reflection\ParameterReflection;
+use PHPStan\Reflection\PassedByReference;
+use PHPStan\Type\Type;
+use PHPStan\Type\TypeCombinator;
+
+final class NativeParameterReflection implements ParameterReflection
+{
+
+	public function __construct(
+		private string $name,
+		private bool $optional,
+		private Type $type,
+		private PassedByReference $passedByReference,
+		private bool $variadic,
+		private ?Type $defaultValue,
+	)
+	{
+	}
+
+	public function getName(): string
+	{
+		return $this->name;
+	}
+
+	public function isOptional(): bool
+	{
+		return $this->optional;
+	}
+
+	public function getType(): Type
+	{
+		return $this->type;
+	}
+
+	public function passedByReference(): PassedByReference
+	{
+		return $this->passedByReference;
+	}
+
+	public function isVariadic(): bool
+	{
+		return $this->variadic;
+	}
+
+	public function getDefaultValue(): ?Type
+	{
+		return $this->defaultValue;
+	}
+
+	public function union(self $other): self
+	{
+		return new self(
+			$this->name,
+			$this->optional && $other->optional,
+			TypeCombinator::union($this->type, $other->type),
+			$this->passedByReference->combine($other->passedByReference),
+			$this->variadic && $other->variadic,
+			$this->optional && $other->optional ? $this->defaultValue : null,
+		);
+	}
+
+}
---
+++ src/Type/Generic/TemplateBenevolentUnionType.php
@@ -0,0 +1,68 @@
+<?php 
+
+namespace PHPStan\Type\Generic;
+return;
+
+use PHPStan\Type\BenevolentUnionType;
+use PHPStan\Type\Type;
+
+/** @api */
+final class TemplateBenevolentUnionType extends BenevolentUnionType implements TemplateType
+{
+
+	/** @use TemplateTypeTrait<BenevolentUnionType> */
+	use TemplateTypeTrait;
+
+	/**
+	 * @param non-empty-string $name
+	 */
+	public function __construct(
+		TemplateTypeScope $scope,
+		TemplateTypeStrategy $templateTypeStrategy,
+		TemplateTypeVariance $templateTypeVariance,
+		string $name,
+		BenevolentUnionType $bound,
+		?Type $default,
+	)
+	{
+		parent::__construct($bound->getTypes());
+
+		$this->scope = $scope;
+		$this->strategy = $templateTypeStrategy;
+		$this->variance = $templateTypeVariance;
+		$this->name = $name;
+		$this->bound = $bound;
+		$this->default = $default;
+	}
+
+	/** @param Type[] $types */
+	public function withTypes(array $types): self
+	{
+		return new self(
+			$this->scope,
+			$this->strategy,
+			$this->variance,
+			$this->name,
+			new BenevolentUnionType($types),
+			$this->default,
+		);
+	}
+
+	public function filterTypes(callable $filterCb): Type
+	{
+		$result = parent::filterTypes($filterCb);
+		if (!$result instanceof TemplateType) {
+			return TemplateTypeFactory::create(
+				$this->getScope(),
+				$this->getName(),
+				$result,
+				$this->getVariance(),
+				$this->getStrategy(),
+				$this->getDefault(),
+			);
+		}
+
+		return $result;
+	}
+
+}
