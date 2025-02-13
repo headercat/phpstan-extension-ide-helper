ffa1a6cfca9b2c3b1de41d3936553dc8c9beba31
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 21:25:19 +0000

    deploy: 1.5.1

---
+++ src/Type/DynamicStaticMethodThrowTypeExtension.php
@@ -0,0 +1,34 @@
+<?php 
+
+namespace PHPStan\Type;
+return;
+
+use PhpParser\Node\Expr\StaticCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+
+/**
+ * This is the interface dynamic throw type extensions implement for static methods.
+ *
+ * To register it in the configuration file use the `phpstan.dynamicStaticMethodThrowTypeExtension` service tag:
+ *
+ * ```
+ * services:
+ * 	-
+ *		class: App\PHPStan\MyExtension
+ *		tags:
+ *			- phpstan.dynamicStaticMethodThrowTypeExtension
+ * ```
+ *
+ * Learn more: https://phpstan.org/developing-extensions/dynamic-throw-type-extensions
+ *
+ * @api
+ */
+interface DynamicStaticMethodThrowTypeExtension
+{
+
+	public function isStaticMethodSupported(MethodReflection $methodReflection): bool;
+
+	public function getThrowTypeFromStaticMethodCall(MethodReflection $methodReflection, StaticCall $methodCall, Scope $scope): ?Type;
+
+}
