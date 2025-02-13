130d06f2f4319745858f3b0d029b20655e0317ef
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 20:51:47 +0000

    deploy: 1.3.1

---
+++ src/DependencyInjection/Type/LazyDynamicThrowTypeExtensionProvider.php
@@ -0,0 +1,34 @@
+<?php 
+
+namespace PHPStan\DependencyInjection\Type;
+return;
+
+use PHPStan\DependencyInjection\Container;
+
+final class LazyDynamicThrowTypeExtensionProvider implements DynamicThrowTypeExtensionProvider
+{
+
+	public const FUNCTION_TAG = 'phpstan.dynamicFunctionThrowTypeExtension';
+	public const METHOD_TAG = 'phpstan.dynamicMethodThrowTypeExtension';
+	public const STATIC_METHOD_TAG = 'phpstan.dynamicStaticMethodThrowTypeExtension';
+
+	public function __construct(private Container $container)
+	{
+	}
+
+	public function getDynamicFunctionThrowTypeExtensions(): array
+	{
+		return $this->container->getServicesByTag(self::FUNCTION_TAG);
+	}
+
+	public function getDynamicMethodThrowTypeExtensions(): array
+	{
+		return $this->container->getServicesByTag(self::METHOD_TAG);
+	}
+
+	public function getDynamicStaticMethodThrowTypeExtensions(): array
+	{
+		return $this->container->getServicesByTag(self::STATIC_METHOD_TAG);
+	}
+
+}
---
+++ src/File/FileExcluderFactory.php
@@ -0,0 +1,47 @@
+<?php 
+
+namespace PHPStan\File;
+return;
+
+use function array_key_exists;
+use function array_merge;
+use function array_unique;
+use function array_values;
+
+final class FileExcluderFactory
+{
+
+	/**
+	 * @param array{analyse?: array<int, string>, analyseAndScan?: array<int, string>} $excludePaths
+	 */
+	public function __construct(
+		private FileExcluderRawFactory $fileExcluderRawFactory,
+		private array $excludePaths,
+	)
+	{
+	}
+
+	public function createAnalyseFileExcluder(): FileExcluder
+	{
+		$paths = [];
+		if (array_key_exists('analyse', $this->excludePaths)) {
+			$paths = $this->excludePaths['analyse'];
+		}
+		if (array_key_exists('analyseAndScan', $this->excludePaths)) {
+			$paths = array_merge($paths, $this->excludePaths['analyseAndScan']);
+		}
+
+		return $this->fileExcluderRawFactory->create(array_values(array_unique($paths)));
+	}
+
+	public function createScanFileExcluder(): FileExcluder
+	{
+		$paths = [];
+		if (array_key_exists('analyseAndScan', $this->excludePaths)) {
+			$paths = $this->excludePaths['analyseAndScan'];
+		}
+
+		return $this->fileExcluderRawFactory->create(array_values(array_unique($paths)));
+	}
+
+}
