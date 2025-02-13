6440b8d714a24c774a65eef6223b44e2b299d82e
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 21:58:00 +0000

    deploy: 1.8.0

---
+++ src/Type/Traits/UndecidedComparisonTypeTrait.php
@@ -0,0 +1,44 @@
+<?php 
+
+namespace PHPStan\Type\Traits;
+return;
+
+use PHPStan\Php\PhpVersion;
+use PHPStan\TrinaryLogic;
+use PHPStan\Type\MixedType;
+use PHPStan\Type\Type;
+
+trait UndecidedComparisonTypeTrait
+{
+
+	public function isSmallerThan(Type $otherType, PhpVersion $phpVersion): TrinaryLogic
+	{
+		return TrinaryLogic::createMaybe();
+	}
+
+	public function isSmallerThanOrEqual(Type $otherType, PhpVersion $phpVersion): TrinaryLogic
+	{
+		return TrinaryLogic::createMaybe();
+	}
+
+	public function getSmallerType(PhpVersion $phpVersion): Type
+	{
+		return new MixedType();
+	}
+
+	public function getSmallerOrEqualType(PhpVersion $phpVersion): Type
+	{
+		return new MixedType();
+	}
+
+	public function getGreaterType(PhpVersion $phpVersion): Type
+	{
+		return new MixedType();
+	}
+
+	public function getGreaterOrEqualType(PhpVersion $phpVersion): Type
+	{
+		return new MixedType();
+	}
+
+}
