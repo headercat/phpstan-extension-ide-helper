95e118ba893f4ba86a73a961525e07efc97ff143
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 19:41:50 +0000

    deploy: 1.10.37

---
+++ src/Reflection/ExtendedMethodReflection.php
@@ -0,0 +1,67 @@
+<?php 
+
+namespace PHPStan\Reflection;
+return;
+
+use PHPStan\TrinaryLogic;
+use PHPStan\Type\Type;
+
+/**
+ * The purpose of this interface is to be able to
+ * answer more questions about methods
+ * without breaking backward compatibility
+ * with existing MethodsClassReflectionExtension.
+ *
+ * Developers are meant to only implement MethodReflection
+ * and its methods in their code.
+ *
+ * New methods on ExtendedMethodReflection will be added
+ * in minor versions.
+ *
+ * @api
+ */
+interface ExtendedMethodReflection extends MethodReflection
+{
+
+	/**
+	 * @return list<ExtendedParametersAcceptor>
+	 */
+	public function getVariants(): array;
+
+	/**
+	 * @internal
+	 */
+	public function getOnlyVariant(): ExtendedParametersAcceptor;
+
+	/**
+	 * @return list<ExtendedParametersAcceptor>|null
+	 */
+	public function getNamedArgumentsVariants(): ?array;
+
+	public function acceptsNamedArguments(): TrinaryLogic;
+
+	public function getAsserts(): Assertions;
+
+	public function getSelfOutType(): ?Type;
+
+	public function returnsByReference(): TrinaryLogic;
+
+	public function isFinalByKeyword(): TrinaryLogic;
+
+	public function isAbstract(): TrinaryLogic|bool;
+
+	/**
+	 * This indicates whether the method has phpstan-pure
+	 * or phpstan-impure annotation above it.
+	 *
+	 * In most cases asking hasSideEffects() is much more practical
+	 * as it also accounts for void return type (method being always impure).
+	 */
+	public function isPure(): TrinaryLogic;
+
+	/**
+	 * @return list<AttributeReflection>
+	 */
+	public function getAttributes(): array;
+
+}
---
+++ src/Rules/FunctionReturnTypeCheck.php
@@ -0,0 +1,112 @@
+<?php 
+
+namespace PHPStan\Rules;
+return;
+
+use Generator;
+use PhpParser\Node;
+use PhpParser\Node\Expr;
+use PHPStan\Analyser\Scope;
+use PHPStan\Type\ErrorType;
+use PHPStan\Type\NeverType;
+use PHPStan\Type\Type;
+use PHPStan\Type\TypeUtils;
+use PHPStan\Type\VerbosityLevel;
+use function sprintf;
+
+final class FunctionReturnTypeCheck
+{
+
+	public function __construct(private RuleLevelHelper $ruleLevelHelper)
+	{
+	}
+
+	/**
+	 * @return list<IdentifierRuleError>
+	 */
+	public function checkReturnType(
+		Scope $scope,
+		Type $returnType,
+		?Expr $returnValue,
+		Node $returnNode,
+		string $emptyReturnStatementMessage,
+		string $voidMessage,
+		string $typeMismatchMessage,
+		string $neverMessage,
+		bool $isGenerator,
+	): array
+	{
+		$returnType = TypeUtils::resolveLateResolvableTypes($returnType);
+
+		if ($returnType instanceof NeverType && $returnType->isExplicit()) {
+			return [
+				RuleErrorBuilder::message($neverMessage)
+					->line($returnNode->getStartLine())
+					->identifier('return.never')
+					->build(),
+			];
+		}
+
+		if ($isGenerator) {
+			$returnType = $returnType->getTemplateType(Generator::class, 'TReturn');
+			if ($returnType instanceof ErrorType) {
+				return [];
+			}
+		}
+
+		$isVoidSuperType = $returnType->isVoid();
+		$verbosityLevel = VerbosityLevel::getRecommendedLevelByType($returnType, null);
+		if ($returnValue === null) {
+			if (!$isVoidSuperType->no()) {
+				return [];
+			}
+
+			return [
+				RuleErrorBuilder::message(sprintf(
+					$emptyReturnStatementMessage,
+					$returnType->describe($verbosityLevel),
+				))
+					->line($returnNode->getStartLine())
+					->identifier('return.empty')
+					->build(),
+			];
+		}
+
+		if ($returnNode instanceof Expr\Yield_ || $returnNode instanceof Expr\YieldFrom) {
+			return [];
+		}
+
+		$returnValueType = $scope->getType($returnValue);
+		$verbosityLevel = VerbosityLevel::getRecommendedLevelByType($returnType, $returnValueType);
+
+		if ($isVoidSuperType->yes()) {
+			return [
+				RuleErrorBuilder::message(sprintf(
+					$voidMessage,
+					$returnValueType->describe($verbosityLevel),
+				))
+					->line($returnNode->getStartLine())
+					->identifier('return.void')
+					->build(),
+			];
+		}
+
+		$accepts = $this->ruleLevelHelper->accepts($returnType, $returnValueType, $scope->isDeclareStrictTypes());
+		if (!$accepts->result) {
+			return [
+				RuleErrorBuilder::message(sprintf(
+					$typeMismatchMessage,
+					$returnType->describe($verbosityLevel),
+					$returnValueType->describe($verbosityLevel),
+				))
+					->line($returnNode->getStartLine())
+					->identifier('return.type')
+					->acceptsReasonsTip($accepts->reasons)
+					->build(),
+			];
+		}
+
+		return [];
+	}
+
+}
