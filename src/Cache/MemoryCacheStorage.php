cb4c21d659e0ed7b15a99ad7f904b969a98f7fa5
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 20:35:50 +0000

    deploy: 1.12.13

---
+++ src/Cache/MemoryCacheStorage.php
@@ -0,0 +1,41 @@
+<?php 
+
+namespace PHPStan\Cache;
+return;
+
+use function var_export;
+
+final class MemoryCacheStorage implements CacheStorage
+{
+
+	/** @var array<string, CacheItem> */
+	private array $storage = [];
+
+	/**
+	 * @return mixed|null
+	 */
+	public function load(string $key, string $variableKey)
+	{
+		if (!isset($this->storage[$key])) {
+			return null;
+		}
+
+		$item = $this->storage[$key];
+		if (!$item->isVariableKeyValid($variableKey)) {
+			return null;
+		}
+
+		return $item->getData();
+	}
+
+	/**
+	 * @param mixed $data
+	 */
+	public function save(string $key, string $variableKey, $data): void
+	{
+		$item = new CacheItem($variableKey, $data);
+		@var_export($item, true);
+		$this->storage[$key] = $item;
+	}
+
+}
---
+++ src/Rules/Arrays/UnpackIterableInArrayRule.php
@@ -0,0 +1,70 @@
+<?php 
+
+namespace PHPStan\Rules\Arrays;
+return;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Node\LiteralArrayNode;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
+use PHPStan\Rules\RuleLevelHelper;
+use PHPStan\Type\ErrorType;
+use PHPStan\Type\Type;
+use PHPStan\Type\VerbosityLevel;
+use function sprintf;
+
+/**
+ * @implements Rule<LiteralArrayNode>
+ */
+final class UnpackIterableInArrayRule implements Rule
+{
+
+	public function __construct(
+		private RuleLevelHelper $ruleLevelHelper,
+	)
+	{
+	}
+
+	public function getNodeType(): string
+	{
+		return LiteralArrayNode::class;
+	}
+
+	public function processNode(Node $node, Scope $scope): array
+	{
+		$errors = [];
+		foreach ($node->getItemNodes() as $itemNode) {
+			$item = $itemNode->getArrayItem();
+			if ($item === null) {
+				continue;
+			}
+			if (!$item->unpack) {
+				continue;
+			}
+
+			$typeResult = $this->ruleLevelHelper->findTypeToCheck(
+				$scope,
+				$item->value,
+				'',
+				static fn (Type $type): bool => $type->isIterable()->yes(),
+			);
+			$type = $typeResult->getType();
+			if ($type instanceof ErrorType) {
+				continue;
+			}
+
+			if ($type->isIterable()->yes()) {
+				continue;
+			}
+
+			$errors[] = RuleErrorBuilder::message(sprintf(
+				'Only iterables can be unpacked, %s given.',
+				$type->describe(VerbosityLevel::typeOnly()),
+			))->identifier('arrayUnpacking.nonIterable')->line($item->getStartLine())->build();
+		}
+
+		return $errors;
+	}
+
+}
