562950f7c4c25c8a6d5026b1eee160809baad716
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 21:40:58 +0000

    deploy: 1.6.9

---
+++ src/Type/Generic/TemplateIntersectionType.php
@@ -0,0 +1,38 @@
+<?php 
+
+namespace PHPStan\Type\Generic;
+return;
+
+use PHPStan\Type\IntersectionType;
+use PHPStan\Type\Type;
+
+/** @api */
+final class TemplateIntersectionType extends IntersectionType implements TemplateType
+{
+
+	/** @use TemplateTypeTrait<IntersectionType> */
+	use TemplateTypeTrait;
+
+	/**
+	 * @param non-empty-string $name
+	 */
+	public function __construct(
+		TemplateTypeScope $scope,
+		TemplateTypeStrategy $templateTypeStrategy,
+		TemplateTypeVariance $templateTypeVariance,
+		string $name,
+		IntersectionType $bound,
+		?Type $default,
+	)
+	{
+		parent::__construct($bound->getTypes());
+
+		$this->scope = $scope;
+		$this->strategy = $templateTypeStrategy;
+		$this->variance = $templateTypeVariance;
+		$this->name = $name;
+		$this->bound = $bound;
+		$this->default = $default;
+	}
+
+}
