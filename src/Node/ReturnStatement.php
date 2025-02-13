85b8fe8a9e677ee2a908fb650743e382157e9ca2
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 22:24:59 +0000

    deploy: 1.9.6

---
+++ src/Node/ReturnStatement.php
@@ -0,0 +1,33 @@
+<?php 
+
+namespace PHPStan\Node;
+return;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Return_;
+use PHPStan\Analyser\Scope;
+
+/**
+ * @api
+ */
+final class ReturnStatement
+{
+
+	private Node\Stmt\Return_ $returnNode;
+
+	public function __construct(private Scope $scope, Return_ $returnNode)
+	{
+		$this->returnNode = $returnNode;
+	}
+
+	public function getScope(): Scope
+	{
+		return $this->scope;
+	}
+
+	public function getReturnNode(): Return_
+	{
+		return $this->returnNode;
+	}
+
+}
---
+++ src/Type/Php/DateFormatFunctionReturnTypeExtension.php
@@ -0,0 +1,42 @@
+<?php 
+
+namespace PHPStan\Type\Php;
+return;
+
+use PhpParser\Node\Expr\FuncCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\FunctionReflection;
+use PHPStan\Type\DynamicFunctionReturnTypeExtension;
+use PHPStan\Type\StringType;
+use PHPStan\Type\Type;
+use function count;
+
+final class DateFormatFunctionReturnTypeExtension implements DynamicFunctionReturnTypeExtension
+{
+
+	public function __construct(private DateFunctionReturnTypeHelper $dateFunctionReturnTypeHelper)
+	{
+	}
+
+	public function isFunctionSupported(FunctionReflection $functionReflection): bool
+	{
+		return $functionReflection->getName() === 'date_format';
+	}
+
+	public function getTypeFromFunctionCall(
+		FunctionReflection $functionReflection,
+		FuncCall $functionCall,
+		Scope $scope,
+	): Type
+	{
+		if (count($functionCall->getArgs()) < 2) {
+			return new StringType();
+		}
+
+		return $this->dateFunctionReturnTypeHelper->getTypeFromFormatType(
+			$scope->getType($functionCall->getArgs()[1]->value),
+			true,
+		);
+	}
+
+}
