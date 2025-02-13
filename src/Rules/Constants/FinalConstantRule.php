8f8da3c56b4860d5598266e3e09641a072d1c5da
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 21:33:57 +0000

    deploy: 1.6.2

---
+++ src/Rules/Constants/FinalConstantRule.php
@@ -0,0 +1,44 @@
+<?php 
+
+namespace PHPStan\Rules\Constants;
+return;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\ClassConst;
+use PHPStan\Analyser\Scope;
+use PHPStan\Php\PhpVersion;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
+
+/** @implements Rule<ClassConst> */
+final class FinalConstantRule implements Rule
+{
+
+	public function __construct(private PhpVersion $phpVersion)
+	{
+	}
+
+	public function getNodeType(): string
+	{
+		return ClassConst::class;
+	}
+
+	public function processNode(Node $node, Scope $scope): array
+	{
+		if (!$node->isFinal()) {
+			return [];
+		}
+
+		if ($this->phpVersion->supportsFinalConstants()) {
+			return [];
+		}
+
+		return [
+			RuleErrorBuilder::message('Final class constants are supported only on PHP 8.1 and later.')
+				->identifier('classConstant.finalNotSupported')
+				->nonIgnorable()
+				->build(),
+		];
+	}
+
+}
---
+++ src/Rules/Generics/PropertyVarianceRule.php
@@ -0,0 +1,56 @@
+<?php 
+
+namespace PHPStan\Rules\Generics;
+return;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Internal\SprintfHelper;
+use PHPStan\Node\ClassPropertyNode;
+use PHPStan\Rules\Rule;
+use PHPStan\Type\Generic\TemplateTypeVariance;
+use function sprintf;
+
+/**
+ * @implements Rule<ClassPropertyNode>
+ */
+final class PropertyVarianceRule implements Rule
+{
+
+	public function __construct(
+		private VarianceCheck $varianceCheck,
+	)
+	{
+	}
+
+	public function getNodeType(): string
+	{
+		return ClassPropertyNode::class;
+	}
+
+	public function processNode(Node $node, Scope $scope): array
+	{
+		$classReflection = $node->getClassReflection();
+
+		if (!$classReflection->hasNativeProperty($node->getName())) {
+			return [];
+		}
+
+		$propertyReflection = $classReflection->getNativeProperty($node->getName());
+
+		if ($propertyReflection->isPrivate()) {
+			return [];
+		}
+
+		$variance = $node->isReadOnly() || $node->isReadOnlyByPhpDoc()
+			? TemplateTypeVariance::createCovariant()
+			: TemplateTypeVariance::createInvariant();
+
+		return $this->varianceCheck->check(
+			$variance,
+			$propertyReflection->getReadableType(),
+			sprintf('in property %s::$%s', SprintfHelper::escapeFormatString($classReflection->getDisplayName()), SprintfHelper::escapeFormatString($node->getName())),
+		);
+	}
+
+}
