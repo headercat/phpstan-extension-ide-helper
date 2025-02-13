15e5751c37ab20da9e7a045e3f297e1556786133
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 22:31:58 +0000

    deploy: 2.0.3

---
+++ src/Reflection/BetterReflection/SourceLocator/FileNodesFetcher.php
@@ -0,0 +1,47 @@
+<?php 
+
+namespace PHPStan\Reflection\BetterReflection\SourceLocator;
+return;
+
+use PhpParser\NodeTraverser;
+use PHPStan\File\FileReader;
+use PHPStan\Parser\Parser;
+use PHPStan\Parser\ParserErrorsException;
+
+final class FileNodesFetcher
+{
+
+	public function __construct(
+		private CachingVisitor $cachingVisitor,
+		private Parser $parser,
+	)
+	{
+	}
+
+	public function fetchNodes(string $fileName): FetchedNodesResult
+	{
+		$nodeTraverser = new NodeTraverser();
+		$nodeTraverser->addVisitor($this->cachingVisitor);
+
+		$contents = FileReader::read($fileName);
+
+		try {
+			$ast = $this->parser->parseFile($fileName);
+		} catch (ParserErrorsException) {
+			return new FetchedNodesResult([], [], []);
+		}
+		$this->cachingVisitor->reset($fileName, $contents);
+		$nodeTraverser->traverse($ast);
+
+		$result = new FetchedNodesResult(
+			$this->cachingVisitor->getClassNodes(),
+			$this->cachingVisitor->getFunctionNodes(),
+			$this->cachingVisitor->getConstantNodes(),
+		);
+
+		$this->cachingVisitor->reset($fileName, $contents);
+
+		return $result;
+	}
+
+}
---
+++ src/Type/ObjectWithoutClassType.php
@@ -0,0 +1,226 @@
+<?php 
+
+namespace PHPStan\Type;
+return;
+
+use PHPStan\PhpDocParser\Ast\Type\IdentifierTypeNode;
+use PHPStan\PhpDocParser\Ast\Type\TypeNode;
+use PHPStan\TrinaryLogic;
+use PHPStan\Type\Traits\NonGeneralizableTypeTrait;
+use PHPStan\Type\Traits\NonGenericTypeTrait;
+use PHPStan\Type\Traits\ObjectTypeTrait;
+use PHPStan\Type\Traits\UndecidedComparisonTypeTrait;
+use function sprintf;
+
+/** @api */
+class ObjectWithoutClassType implements SubtractableType
+{
+
+	use ObjectTypeTrait;
+	use NonGenericTypeTrait;
+	use UndecidedComparisonTypeTrait;
+	use NonGeneralizableTypeTrait;
+
+	private ?Type $subtractedType;
+
+	/** @api */
+	public function __construct(
+		?Type $subtractedType = null,
+	)
+	{
+		if ($subtractedType instanceof NeverType) {
+			$subtractedType = null;
+		}
+
+		$this->subtractedType = $subtractedType;
+	}
+
+	public function getReferencedClasses(): array
+	{
+		return [];
+	}
+
+	public function getObjectClassNames(): array
+	{
+		return [];
+	}
+
+	public function getObjectClassReflections(): array
+	{
+		return [];
+	}
+
+	public function accepts(Type $type, bool $strictTypes): AcceptsResult
+	{
+		if ($type instanceof CompoundType) {
+			return $type->isAcceptedBy($this, $strictTypes);
+		}
+
+		return AcceptsResult::createFromBoolean(
+			$type instanceof self || $type instanceof ObjectShapeType || $type->getObjectClassNames() !== [],
+		);
+	}
+
+	public function isSuperTypeOf(Type $type): IsSuperTypeOfResult
+	{
+		if ($type instanceof CompoundType) {
+			return $type->isSubTypeOf($this);
+		}
+
+		if ($type instanceof self) {
+			if ($this->subtractedType === null) {
+				return IsSuperTypeOfResult::createYes();
+			}
+			if ($type->subtractedType !== null) {
+				$isSuperType = $type->subtractedType->isSuperTypeOf($this->subtractedType);
+				if ($isSuperType->yes()) {
+					return $isSuperType;
+				}
+			}
+
+			return IsSuperTypeOfResult::createMaybe();
+		}
+
+		if ($type instanceof ObjectShapeType) {
+			return IsSuperTypeOfResult::createYes();
+		}
+
+		if ($type->getObjectClassNames() === []) {
+			return IsSuperTypeOfResult::createNo();
+		}
+
+		if ($this->subtractedType === null) {
+			return IsSuperTypeOfResult::createYes();
+		}
+
+		return $this->subtractedType->isSuperTypeOf($type)->negate();
+	}
+
+	public function equals(Type $type): bool
+	{
+		if (!$type instanceof self) {
+			return false;
+		}
+
+		if ($this->subtractedType === null) {
+			if ($type->subtractedType === null) {
+				return true;
+			}
+
+			return false;
+		}
+
+		if ($type->subtractedType === null) {
+			return false;
+		}
+
+		return $this->subtractedType->equals($type->subtractedType);
+	}
+
+	public function describe(VerbosityLevel $level): string
+	{
+		return $level->handle(
+			static fn (): string => 'object',
+			static fn (): string => 'object',
+			function () use ($level): string {
+				$description = 'object';
+				if ($this->subtractedType !== null) {
+					$description .= $this->subtractedType instanceof UnionType
+						? sprintf('~(%s)', $this->subtractedType->describe($level))
+						: sprintf('~%s', $this->subtractedType->describe($level));
+				}
+
+				return $description;
+			},
+		);
+	}
+
+	public function isOffsetAccessLegal(): TrinaryLogic
+	{
+		return TrinaryLogic::createMaybe();
+	}
+
+	public function getEnumCases(): array
+	{
+		return [];
+	}
+
+	public function subtract(Type $type): Type
+	{
+		if ($type instanceof self) {
+			return new NeverType();
+		}
+		if ($this->subtractedType !== null) {
+			$type = TypeCombinator::union($this->subtractedType, $type);
+		}
+
+		return new self($type);
+	}
+
+	public function getTypeWithoutSubtractedType(): Type
+	{
+		return new self();
+	}
+
+	public function changeSubtractedType(?Type $subtractedType): Type
+	{
+		return new self($subtractedType);
+	}
+
+	public function getSubtractedType(): ?Type
+	{
+		return $this->subtractedType;
+	}
+
+	public function traverse(callable $cb): Type
+	{
+		$subtractedType = $this->subtractedType !== null ? $cb($this->subtractedType) : null;
+
+		if ($subtractedType !== $this->subtractedType) {
+			return new self($subtractedType);
+		}
+
+		return $this;
+	}
+
+	public function traverseSimultaneously(Type $right, callable $cb): Type
+	{
+		if ($this->subtractedType === null) {
+			return $this;
+		}
+
+		return new self();
+	}
+
+	public function tryRemove(Type $typeToRemove): ?Type
+	{
+		if ($this->isSuperTypeOf($typeToRemove)->yes()) {
+			return $this->subtract($typeToRemove);
+		}
+
+		return null;
+	}
+
+	public function exponentiate(Type $exponent): Type
+	{
+		if (!$exponent instanceof NeverType && !$this->isSuperTypeOf($exponent)->no()) {
+			return TypeCombinator::union($this, $exponent);
+		}
+
+		return new BenevolentUnionType([
+			new FloatType(),
+			new IntegerType(),
+		]);
+	}
+
+	public function getFiniteTypes(): array
+	{
+		return [];
+	}
+
+	public function toPhpDocNode(): TypeNode
+	{
+		return new IdentifierTypeNode('object');
+	}
+
+}
