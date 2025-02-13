905ef7c2e1c884b852f5b7f5e79dcae512965853
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 19:36:55 +0000

    deploy: 1.10.32

---
+++ src/Internal/DirectoryCreatorException.php
@@ -0,0 +1,25 @@
+<?php 
+
+namespace PHPStan\Internal;
+return;
+
+use Exception;
+use function error_get_last;
+use function is_null;
+use function sprintf;
+
+final class DirectoryCreatorException extends Exception
+{
+
+	public function __construct(public readonly string $directory)
+	{
+		$error = error_get_last();
+
+		parent::__construct(sprintf(
+			'Failed to create directory "%s" (%s).',
+			$directory,
+			is_null($error) ? 'unknown cause' : $error['message'],
+		));
+	}
+
+}
