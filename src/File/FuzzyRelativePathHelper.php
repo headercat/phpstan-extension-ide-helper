02148070b9ffbc6d78261f3f8b8ec08d20ceb172
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 22:12:56 +0000

    deploy: 1.9.11

---
+++ src/File/FuzzyRelativePathHelper.php
@@ -0,0 +1,119 @@
+<?php 
+
+namespace PHPStan\File;
+return;
+
+use function count;
+use function explode;
+use function implode;
+use function in_array;
+use function ltrim;
+use function realpath;
+use function str_ends_with;
+use function str_starts_with;
+use function strlen;
+use function substr;
+use const DIRECTORY_SEPARATOR;
+
+final class FuzzyRelativePathHelper implements RelativePathHelper
+{
+
+	private string $directorySeparator;
+
+	private ?string $pathToTrim = null;
+
+	/**
+	 * @param string[] $analysedPaths
+	 * @param non-empty-string|null $directorySeparator
+	 */
+	public function __construct(
+		private RelativePathHelper $fallbackRelativePathHelper,
+		string $currentWorkingDirectory,
+		array $analysedPaths,
+		?string $directorySeparator = null,
+	)
+	{
+		if ($directorySeparator === null) {
+			$directorySeparator = DIRECTORY_SEPARATOR;
+		}
+
+		$this->directorySeparator = $directorySeparator;
+		$pathBeginning = null;
+		$pathToTrimArray = null;
+		$trimBeginning = static function (string $path): array {
+			if (str_starts_with($path, '/')) {
+				return [
+					'/',
+					substr($path, 1),
+				];
+			} elseif (substr($path, 1, 1) === ':') {
+				return [
+					substr($path, 0, 3),
+					substr($path, 3),
+				];
+			}
+
+			return ['', $path];
+		};
+
+		if (
+			!in_array($currentWorkingDirectory, ['', '/'], true)
+			&& !(strlen($currentWorkingDirectory) === 3 && substr($currentWorkingDirectory, 1, 1) === ':')
+		) {
+			[$pathBeginning, $currentWorkingDirectory] = $trimBeginning($currentWorkingDirectory);
+
+			$pathToTrimArray = explode($directorySeparator, $currentWorkingDirectory);
+		}
+		foreach ($analysedPaths as $pathNumber => $path) {
+			[$tempPathBeginning, $path] = $trimBeginning($path);
+
+			$pathArray = explode($directorySeparator, $path);
+			$pathTempParts = [];
+			$pathArraySize = count($pathArray);
+			foreach ($pathArray as $i => $pathPart) {
+				if ($i === $pathArraySize - 1 && str_ends_with($pathPart, '.php')) {
+					continue;
+				}
+				if (!isset($pathToTrimArray[$i])) {
+					if ($pathNumber !== 0) {
+						$pathToTrimArray = $pathTempParts;
+						continue 2;
+					}
+				} elseif ($pathToTrimArray[$i] !== $pathPart) {
+					$pathToTrimArray = $pathTempParts;
+					continue 2;
+				}
+
+				$pathTempParts[] = $pathPart;
+			}
+
+			$pathBeginning = $tempPathBeginning;
+			$pathToTrimArray = $pathTempParts;
+		}
+
+		if ($pathToTrimArray === null || count($pathToTrimArray) === 0) {
+			return;
+		}
+
+		$pathToTrim = $pathBeginning . implode($directorySeparator, $pathToTrimArray);
+		$realPathToTrim = realpath($pathToTrim);
+		if ($realPathToTrim !== false) {
+			$pathToTrim = $realPathToTrim;
+		}
+
+		$this->pathToTrim = $pathToTrim;
+	}
+
+	public function getRelativePath(string $filename): string
+	{
+		if (
+			$this->pathToTrim !== null
+			&& str_starts_with($filename, $this->pathToTrim)
+		) {
+			return ltrim(substr($filename, strlen($this->pathToTrim)), $this->directorySeparator);
+		}
+
+		return $this->fallbackRelativePathHelper->getRelativePath($filename);
+	}
+
+}
---
+++ src/Reflection/FunctionReflection.php
@@ -0,0 +1,66 @@
+<?php 
+
+namespace PHPStan\Reflection;
+return;
+
+use PHPStan\TrinaryLogic;
+use PHPStan\Type\Type;
+
+/** @api */
+interface FunctionReflection
+{
+
+	public function getName(): string;
+
+	public function getFileName(): ?string;
+
+	/**
+	 * @return list<ExtendedParametersAcceptor>
+	 */
+	public function getVariants(): array;
+
+	/**
+	 * @internal
+	 */
+	public function getOnlyVariant(): ExtendedParametersAcceptor;
+
+	/**
+	 * @return list<ExtendedParametersAcceptor>|null
+	 */
+	public function getNamedArgumentsVariants(): ?array;
+
+	public function acceptsNamedArguments(): TrinaryLogic;
+
+	public function isDeprecated(): TrinaryLogic;
+
+	public function getDeprecatedDescription(): ?string;
+
+	public function isInternal(): TrinaryLogic;
+
+	public function getThrowType(): ?Type;
+
+	public function hasSideEffects(): TrinaryLogic;
+
+	public function isBuiltin(): bool;
+
+	public function getAsserts(): Assertions;
+
+	public function getDocComment(): ?string;
+
+	public function returnsByReference(): TrinaryLogic;
+
+	/**
+	 * This indicates whether the function has phpstan-pure
+	 * or phpstan-impure annotation above it.
+	 *
+	 * In most cases asking hasSideEffects() is much more practical
+	 * as it also accounts for void return type (method being always impure).
+	 */
+	public function isPure(): TrinaryLogic;
+
+	/**
+	 * @return list<AttributeReflection>
+	 */
+	public function getAttributes(): array;
+
+}
