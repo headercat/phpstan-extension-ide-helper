99060cd1148e417118c93bb19765afaef02f5393
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 21:32:58 +0000

    deploy: 1.6.1

---
+++ src/Node/Property/PropertyWrite.php
@@ -0,0 +1,38 @@
+<?php 
+
+namespace PHPStan\Node\Property;
+return;
+
+use PhpParser\Node\Expr\PropertyFetch;
+use PhpParser\Node\Expr\StaticPropertyFetch;
+use PHPStan\Analyser\Scope;
+
+/**
+ * @api
+ */
+final class PropertyWrite
+{
+
+	public function __construct(private PropertyFetch|StaticPropertyFetch $fetch, private Scope $scope, private bool $promotedPropertyWrite)
+	{
+	}
+
+	/**
+	 * @return PropertyFetch|StaticPropertyFetch
+	 */
+	public function getFetch()
+	{
+		return $this->fetch;
+	}
+
+	public function getScope(): Scope
+	{
+		return $this->scope;
+	}
+
+	public function isPromotedPropertyWrite(): bool
+	{
+		return $this->promotedPropertyWrite;
+	}
+
+}
