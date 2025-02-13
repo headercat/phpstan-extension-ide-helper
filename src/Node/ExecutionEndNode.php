53030661fb5fa4a69fe1d3ea18873fe55a544720
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 20:03:47 +0000

    deploy: 1.10.57

---
+++ src/Node/ExecutionEndNode.php
@@ -0,0 +1,53 @@
+<?php 
+
+namespace PHPStan\Node;
+return;
+
+use PhpParser\Node;
+use PhpParser\NodeAbstract;
+use PHPStan\Analyser\StatementResult;
+
+/**
+ * @api
+ */
+final class ExecutionEndNode extends NodeAbstract implements VirtualNode
+{
+
+	public function __construct(
+		private Node\Stmt $node,
+		private StatementResult $statementResult,
+		private bool $hasNativeReturnTypehint,
+	)
+	{
+		parent::__construct($node->getAttributes());
+	}
+
+	public function getNode(): Node\Stmt
+	{
+		return $this->node;
+	}
+
+	public function getStatementResult(): StatementResult
+	{
+		return $this->statementResult;
+	}
+
+	public function hasNativeReturnTypehint(): bool
+	{
+		return $this->hasNativeReturnTypehint;
+	}
+
+	public function getType(): string
+	{
+		return 'PHPStan_Node_ExecutionEndNode';
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public function getSubNodeNames(): array
+	{
+		return [];
+	}
+
+}
---
+++ src/Rules/Exceptions/TooWideFunctionThrowTypeRule.php
@@ -0,0 +1,52 @@
+<?php 
+
+namespace PHPStan\Rules\Exceptions;
+return;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Node\FunctionReturnStatementsNode;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
+use function sprintf;
+
+/**
+ * @implements Rule<FunctionReturnStatementsNode>
+ */
+final class TooWideFunctionThrowTypeRule implements Rule
+{
+
+	public function __construct(private TooWideThrowTypeCheck $check)
+	{
+	}
+
+	public function getNodeType(): string
+	{
+		return FunctionReturnStatementsNode::class;
+	}
+
+	public function processNode(Node $node, Scope $scope): array
+	{
+		$statementResult = $node->getStatementResult();
+		$functionReflection = $node->getFunctionReflection();
+
+		$throwType = $functionReflection->getThrowType();
+		if ($throwType === null) {
+			return [];
+		}
+
+		$errors = [];
+		foreach ($this->check->check($throwType, $statementResult->getThrowPoints()) as $throwClass) {
+			$errors[] = RuleErrorBuilder::message(sprintf(
+				'Function %s() has %s in PHPDoc @throws tag but it\'s not thrown.',
+				$functionReflection->getName(),
+				$throwClass,
+			))
+				->identifier('throws.unusedType')
+				->build();
+		}
+
+		return $errors;
+	}
+
+}
