faaa067bd83efd2e383b47713702546b7ec5ed74
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-02-13 19:32:45 +0000

    deploy: 1.10.29

---
+++ src/Internal/DirectoryCreator.php
@@ -0,0 +1,37 @@
+<?php 
+
+namespace PHPStan\Internal;
+return;
+
+use function clearstatcache;
+use function is_dir;
+use function mkdir;
+
+final class DirectoryCreator
+{
+
+	/**
+	 * @throws DirectoryCreatorException if unable to create directory.
+	 */
+	public static function ensureDirectoryExists(
+		string $directory,
+		int $mode = 0755,
+		bool $recursive = true,
+	): void
+	{
+		if (is_dir($directory)) {
+			return;
+		}
+
+		if (@mkdir($directory, $mode, $recursive)) {
+			return;
+		}
+
+		clearstatcache();
+
+		if (!is_dir($directory)) {
+			throw new DirectoryCreatorException($directory);
+		}
+	}
+
+}
