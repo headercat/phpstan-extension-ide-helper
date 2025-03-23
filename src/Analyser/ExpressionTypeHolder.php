ddade4912eced0ad63708958fd21cdf3e549c157
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-03-23 14:11:55 +0000

    deploy: 2.1.9

---
+++ src/Analyser/ExpressionTypeHolder.php
@@ -0,0 +1,70 @@
+<?php 
+
+namespace PHPStan\Analyser;
+return;
+
+use PhpParser\Node\Expr;
+use PHPStan\TrinaryLogic;
+use PHPStan\Type\Type;
+use PHPStan\Type\TypeCombinator;
+
+final class ExpressionTypeHolder
+{
+
+	public function __construct(private Expr $expr, private Type $type, private TrinaryLogic $certainty)
+	{
+	}
+
+	public static function createYes(Expr $expr, Type $type): self
+	{
+		return new self($expr, $type, TrinaryLogic::createYes());
+	}
+
+	public static function createMaybe(Expr $expr, Type $type): self
+	{
+		return new self($expr, $type, TrinaryLogic::createMaybe());
+	}
+
+	public function equals(self $other): bool
+	{
+		if (!$this->certainty->equals($other->certainty)) {
+			return false;
+		}
+
+		return $this->type->equals($other->type);
+	}
+
+	public function and(self $other): self
+	{
+		if ($this->type->equals($other->type)) {
+			if ($this->certainty->equals($other->certainty)) {
+				return $this;
+			}
+
+			$type = $this->type;
+		} else {
+			$type = TypeCombinator::union($this->type, $other->type);
+		}
+		return new self(
+			$this->expr,
+			$type,
+			$this->certainty->and($other->certainty),
+		);
+	}
+
+	public function getExpr(): Expr
+	{
+		return $this->expr;
+	}
+
+	public function getType(): Type
+	{
+		return $this->type;
+	}
+
+	public function getCertainty(): TrinaryLogic
+	{
+		return $this->certainty;
+	}
+
+}
---
+++ src/Rules/PhpDoc/RequireExtendsDefinitionClassRule.php
@@ -0,0 +1,51 @@
+<?php 
+
+namespace PHPStan\Rules\PhpDoc;
+return;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Node\InClassNode;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
+use function count;
+use function sprintf;
+
+/**
+ * @implements Rule<InClassNode>
+ */
+final class RequireExtendsDefinitionClassRule implements Rule
+{
+
+	public function __construct(
+		private RequireExtendsCheck $requireExtendsCheck,
+	)
+	{
+	}
+
+	public function getNodeType(): string
+	{
+		return InClassNode::class;
+	}
+
+	public function processNode(Node $node, Scope $scope): array
+	{
+		$classReflection = $node->getClassReflection();
+		$extendsTags = $classReflection->getRequireExtendsTags();
+
+		if (count($extendsTags) === 0) {
+			return [];
+		}
+
+		if (!$classReflection->isInterface()) {
+			return [
+				RuleErrorBuilder::message('PHPDoc tag @phpstan-require-extends is only valid on trait or interface.')
+					->identifier(sprintf('requireExtends.on%s', $classReflection->getClassTypeDescription()))
+					->build(),
+			];
+		}
+
+		return $this->requireExtendsCheck->checkExtendsTags($node, $extendsTags);
+	}
+
+}
