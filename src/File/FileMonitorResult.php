4927cdb7e6d5789623f353f745c9128ea57cae3e
Ji Yong, Kim <jiyong.kim@headercat.com>
2025-03-23 14:08:19 +0000

    deploy: 1.12.22

---
+++ src/File/FileMonitorResult.php
@@ -0,0 +1,45 @@
+<?php 
+
+namespace PHPStan\File;
+return;
+
+use function count;
+
+final class FileMonitorResult
+{
+
+	/**
+	 * @param string[] $newFiles
+	 * @param string[] $changedFiles
+	 * @param string[] $deletedFiles
+	 */
+	public function __construct(
+		private array $newFiles,
+		private array $changedFiles,
+		private array $deletedFiles,
+		private int $totalFilesCount,
+	)
+	{
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public function getChangedFiles(): array
+	{
+		return $this->changedFiles;
+	}
+
+	public function hasAnyChanges(): bool
+	{
+		return count($this->newFiles) > 0
+			|| count($this->changedFiles) > 0
+			|| count($this->deletedFiles) > 0;
+	}
+
+	public function getTotalFilesCount(): int
+	{
+		return $this->totalFilesCount;
+	}
+
+}
